76e3e0bccc8dd0d76bc19334f7a20a6c9902c52a
Theo <theo.guil76@outlook.fr>
2023-10-04 18:15:18 +0200

    [Kayak] Fin scrapping Booking

--- Kayak/src/scrapy_booking.py
+++ Kayak/src/scrapy_booking.py
@@ -1,36 +1,35 @@
 import os
 import logging
 import scrapy
+import json
+import sys
 from scrapy.crawler import CrawlerProcess
 
 
 class ScrapyBooking(scrapy.Spider):
-    # Name of your spider
     name = "booking"
 
-    # Starting URL
     start_urls = ["https://www.booking.com/index.fr.html"]
 
     def parse(self, response):
         return scrapy.FormRequest.from_response(
             response,
             formdata={
-                "ss": "Rouen",
-                "ssne": "Rouen",
-                "ssne_untouched": "Rouen",
+                "ss": city,
+                "ssne": city,
+                "ssne_untouched": city,
                 "checkin": "2023-12-01",
                 "checkout": "2023-12-02",
             },
-            # Function to be called once logged in
             callback=self.after_search,
         )
 
     # Callback used after search
     def after_search(self, response):
         print(response.url)
-        with open("./src/rouen-booking.html", "w") as fp:
-            # uncomment if you want empty file
-            fp.write(response.text)
+
+        # with open("./src/rouen-booking.html", "w") as fp:
+        #     fp.write(response.text)
 
         hotels = response.xpath(
             '//*[@id="search_results_table"]/div[2]/div/div/div[3]/div'
@@ -38,74 +37,75 @@ class ScrapyBooking(scrapy.Spider):
 
         for hotel in hotels:
             try:
-                name = hotel.xpath(
-                    "div[1]/div[2]/div/div[1]/div/div[1]/div/div[1]/div[1]/h3/a/div[1]/text()"
-                ).get()
                 url = hotel.xpath(
                     "div[1]/div[2]/div/div[1]/div/div[1]/div/div[1]/div/h3/a"
                 ).attrib["href"]
-                score = hotel.xpath(
-                    "div[1]/div[2]/div/div[1]/div/div[2]/div/div/div/a/span/div/div[1]/text()"
-                ).get()
-                # try:
-                #     desc = hotel.xpath(
-                #         "div[1]/div[2]/div/div/div/div[1]/div/div[4]/text()"
-                #     ).get()
-                # except:
-                #     desc = None
 
-                try:
-                    price = (
-                        hotel.xpath(
-                            "div[1]/div[2]/div/div[2]/div[2]/div/div[1]/div[2]/text()"
-                        )
-                        .get()
-                    )
-                except:
-                    price = None
+                yield scrapy.Request(url, callback=self.get_detail)
 
-                print(price)
-
-                yield {
-                    "name": name,
-                    "url": url,
-                    "score": score,
-                    "description": None,
-                    "latitude": None,
-                    "longitude": None,
-                    "price": price,
-                }
-            except Exception as e:
+            except Exception:
                 pass
 
-        # Select the NEXT button and store it in next_page
-        # try:
-        #     next_page = response.xpath('/html/body/div/div[2]/div[1]/nav/ul/li[@class="next"]/a').attrib["href"]
-        # except KeyError:
-        #     # In the last page, there won't be any "href" and a KeyError will be raised
-        #     logging.info('No next page. Terminating crawling process.')
-        # else:
-        #     # If a next page is found, execute the parse method once again
-        #     yield response.follow(next_page, callback=self.after_login)
+    # Callback use to scrap detail page
+    def get_detail(self, response):
+        print(response.url)
 
+        name = response.xpath('//*[@id="hp_hotel_name"]/div/h2/text()').get()
+
+        score = response.xpath(
+            '//*[@id="js--hp-gallery-scorecard"]/a/div/div/div/div[1]/text()'
+        ).get()
+
+        desc = response.xpath(
+            '//*[@id="property_description_content"]/div/p/text()'
+        ).get()
+
+        latlong = response.xpath(
+            '//*[@id="hotel_sidebar_static_map"]/@data-atlas-latlng'
+        ).get()
+
+        price = response.xpath(
+            '//*[@id="hprt-table"]/tbody/tr[1]/td[3]/div/div/div[1]/div[2]/div/span/text()'
+        ).get()
+
+        yield {
+            "name": name,
+            "url": response.url,
+            "score": score,
+            "description": desc,
+            "latitude": latlong.split(",")[0],
+            "longitude": latlong.split(",")[1],
+            "price": price,
+            "city": city
+        }
+
+city = sys.argv[1].replace('_', ' ')
+print(city)
 
 # Name of the file where the results will be saved
-filename = "hotel-rouen.json"
+filename = f"hotel-{city}.json"
 
 # If file already exists, delete it before crawling (because Scrapy will
 # concatenate the last and new results otherwise)
 if filename in os.listdir("src/"):
     os.remove("src/" + filename)
 
-## More info on built-in settings => https://docs.scrapy.org/en/latest/topics/settings.html?highlight=settings#settings
 process = CrawlerProcess(
-    settings={
+    settings = {
         "USER_AGENT": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36",
         "LOG_LEVEL": logging.INFO,
         "FEEDS": {
-            "src/" + filename: {"format": "json"},
+            "src/" + filename: {
+                "format": "json"
+            },
+            "s3://jedha-project-kayak/booking/" + filename: {
+                "format": "json"
+            },
         },
-        "DOWNLOAD_DELAY": 5,
+        "FEED_EXPORT_ENCODING": "utf-8",
+        "AWS_ACCESS_KEY_ID": 'AWS_ACCESS_KEY_ID',
+        "AWS_SECRET_ACCESS_KEY": 'AWS_SECRET_ACCESS_KEY',
+        "DOWNLOAD_DELAY": 2,
         "AUTOTHROTTLE_ENABLED": True,
         "COOKIES_ENABLED": False,
     }
@@ -114,27 +114,3 @@ process = CrawlerProcess(
 # Start the crawling using the spider you defined above
 process.crawl(ScrapyBooking)
 process.start()
-
-
-"""
-for quote in quotes:
-            yield {
-                "city": f"{city}",
-                "name": quote.xpath(
-                    'div[1]/div[2]/div/div/div/div[1]/div/div[1]/div/h3/a/div[@data-testid="title"]/text()'
-                ).get(),
-                "url": quote.xpath(
-                    'div[1]/div[2]/div/div/div/div[1]/div/div[1]/div/h3/a[@data-testid="title-link"]/@href'
-                ).get(),
-                "note": quote.xpath(
-                    "div[1]/div[2]/div/div/div/div[2]/div/div[1]/div/a/span/div/div[1]/text()"
-                ).get(),
-            }
-def parse(self, response):
-        yield {
-            "latlong": response.css("a::attr(data-atlas-latlng)").get(),
-            "description": response.xpath(
-                '//div[@id="property_description_content"]/div/p/text()'
-            ).get(),
-        }
-"""
